f6bdf95e281ff84c50ea33a2d08abccc912fab65
GS05-44-MatsumotoDaisuke <da.mats1990@gmail.com>
2017-07-27 23:47:13 +0900

    ニュースを表示

---
+++ NewsReader/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 松本大佑 on 2017/07/27.
+//  Copyright © 2017年 daisuke.matsumoto. All rights reserved.
+//
+
+class Item {
+  var title = ""
+  var link = ""
+}
+
